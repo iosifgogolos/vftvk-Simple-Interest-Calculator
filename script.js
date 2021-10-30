cc74f8923487f74501b07e5b632835fd5618c199
iosifgogolos <82125428+iosifgogolos@users.noreply.github.com>
2021-10-30 19:45:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+  
+    if (p <= 0){window.alert("Enter a positive number")
+document.getElementById("principal").focus();}
+   else {         
+    var interest = p * r * y / 100;
+    var yearNow = new Date().getFullYear();
+    var yearsFromNow = (+yearNow) + (+y);
+var result = document.getElementById("result");
+    result.innerHTML = 
+        '<div>' +
+            'If you deposit <span class="number" id="principalResult\">' + p + '</span>,<br/>' +
+            'at an interest rate of <span class="number" id="rateResult">' + r + '%</span>.<br/>' +
+            'You will receive an amount of <span class="number" id="interestResult">' + interest + '</span>,<br/>' +
+            'in the year <span class="number" id="futureYearResult">' + yearsFromNow + '</span>.<br/>' +
+        '</div>'}  
 }
-        
+function slider()
+{ 
+    var sv = document.getElementById("rate").value;
+document.getElementById("slider_value").innerHTML = sv + "%";}
